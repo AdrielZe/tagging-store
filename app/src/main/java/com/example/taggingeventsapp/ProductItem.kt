8b4089898612d51d10960728f901f8a7b4fdd2a9
adrielze <oadriel14@gmail.com>
2025-05-20 09:46:45 -0300

    adding button color

--- app/src/main/java/com/example/taggingeventsapp/ProductItem.kt
+++ app/src/main/java/com/example/taggingeventsapp/ProductItem.kt
@@ -7,9 +7,15 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -22,11 +28,13 @@ fun ProductItem(
     onAddToWishlist: (Product) -> Unit,
     onClick: (Product) -> Unit,
 ) {
+    var isOnWishlist by remember { mutableStateOf(product.isOnWishList) }
+
     Column(
         modifier = Modifier
             .width(200.dp)
             .padding(8.dp)
-            .clickable{onClick(product)}
+            .clickable { onClick(product) }
     ) {
         AsyncImage(
             model = product.imageUrl,
@@ -41,7 +49,17 @@ fun ProductItem(
         Button(onClick = { onAddToCart(product) }, modifier = Modifier.padding(top = 4.dp)) {
             Text("Adicionar")
         }
-        Button(onClick = { onAddToWishlist(product) }, modifier = Modifier.padding(top = 4.dp)) {
+        Button(
+            onClick = {
+                isOnWishlist = !isOnWishlist
+                product.isOnWishList = isOnWishlist
+                onAddToWishlist(product)
+            },
+            modifier = Modifier.padding(top = 4.dp),
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = if (isOnWishlist) Color.Blue else Color.LightGray
+            )
+        ) {
             Text("❤\uFE0F")
         }
     }
